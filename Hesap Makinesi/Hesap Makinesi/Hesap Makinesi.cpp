2d7c4f961575d213b06998252c2ed9aaf00395d7
Sefa Yusuf Kütük <sefayusufkutuk@gmail.com>
2026-06-30 17:51:14 +0300

    Add files via upload

---
+++ Hesap Makinesi/Hesap Makinesi/Hesap Makinesi.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	double sayi1, sayi2;
+
+	double sonuc;
+
+	char Operator;
+
+	cout << "Hesap Makinesine Hos Geldiniz!" << endl;
+
+	cout << "Birinci sayiyi giriniz: ";
+	cin >> sayi1;
+
+	cout << "ikinci sayiyi giriniz: ";
+	cin >> sayi2;
+
+	cout << "Operatoru gir:";
+	cin >> Operator;
+
+	switch (Operator)
+	{
+	case '+' :
+		sonuc = sayi1 + sayi2;
+		    break;
+
+	case '-' :
+			sonuc = sayi1 - sayi2;
+			break;
+	case '*' :
+			sonuc = sayi1 * sayi2;
+			break;
+	case '/' :
+		if (sayi2 == 0)
+		{
+			cout << "Sifira Bolunmez! " << endl;
+			return 0;
+		}
+		else
+			sonuc = sayi1 / sayi2;
+		break;
+		
+         
+
+	default:
+		cout << "Yanlis Operator girdin.";
+		return 0;
+
+	}
+	
+	cout << "Sonuc:" << sonuc;
+
+
+	return 0;
+}
