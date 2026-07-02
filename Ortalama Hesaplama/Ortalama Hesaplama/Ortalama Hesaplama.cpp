1eaf088516bf2c3f0e84400413700ca4ab47ef00
Sefa Yusuf Kütük <sefayusufkutuk@gmail.com>
2026-07-02 14:19:54 +0300

    Ortalama Hesaplama

---
+++ Ortalama Hesaplama/Ortalama Hesaplama/Ortalama Hesaplama.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float sayi1, sayi2, sayi3, sayi4;
+
+	float sonuc;
+
+	cout << "Merhaba bugun Sefa'nin bilgisayar icin matematik dersindeki ortalamasini hesaplicaz." << endl;
+
+	cout << "ilk notu giriniz." << endl;
+	cin >> sayi1;
+
+	cout << "ikinci notu giriniz." << endl;
+	cin >> sayi2;
+
+	cout << "Ucuncu notu giriniz." << endl;
+	cin >> sayi3;
+
+	cout << "Son olarak dorduncu notu giriniz." << endl;
+	cin >> sayi4;
+
+	cout << "Oncelikle soyle yapiyoruz ilk uc notun %20'si aliniyor son notun ise %40'i ona gore yapicaz." << endl;
+
+	cout << "Cout kismina yazdigim yazilari yorum satiri olarak ekleyebiliyorum biliyorum ama biraz renk katmak istedim. :) " << endl;
+
+	sonuc = ((sayi1 + sayi2 + sayi3) * 20 / 100 + (sayi4 * 40 / 100));
+
+	cout << "Sonucumuz: " << sonuc << endl;
+
+
+
+	return 0;
+}
