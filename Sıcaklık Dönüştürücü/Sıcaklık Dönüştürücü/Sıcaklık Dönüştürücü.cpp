a13010c0c9824b776be614dea70fedbef55b745b
Sefa Yusuf Kütük <sefayusufkutuk@gmail.com>
2026-07-03 15:41:05 +0300

    Add files via upload

---
+++ Sıcaklık Dönüştürücü/Sıcaklık Dönüştürücü/Sıcaklık Dönüştürücü.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double celcius, fahrenheit;
+
+	cout << "Sicaklik Donusturucuye Hos Geldiniz!" << endl;
+
+	cout << "Dereceyi giriniz." << endl;
+	cin >> celcius;
+
+	cout << "Donusturme islemimiz su sekilde olacak celcius * 1.8 + 32'dir." << endl;
+
+	fahrenheit = celcius * 1.8 + 32; 
+
+	cout << fahrenheit << endl;
+
+
+
+
+
+	return 0;
+}
