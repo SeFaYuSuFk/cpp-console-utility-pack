a065757a4a49e6c99d5a5fc4ef554034a5677aa3
Sefa Yusuf Kütük <sefayusufkutuk@gmail.com>
2026-07-03 15:41:50 +0300

    Add files via upload

---
+++ En Büyük Sayı Bulma/En Büyük Sayı Bulma/En Büyük Sayı Bulma.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int dizi[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	
+	int kucuk = dizi[0];
+	int buyuk = dizi[0];
+
+	for (int i = 0; i < 10; i++)
+	{
+
+		if (kucuk > dizi[i])
+			kucuk = dizi[i];
+		if (buyuk < dizi[i])
+			buyuk = dizi[i];
+
+
+
+	}
+
+
+	cout << "En buyuk deger: " << buyuk << endl;
+
+	cout << "En kucuk deger: " << kucuk << endl;
+
+
+
+
+	return 0;
+}
